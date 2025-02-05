80c7fe63eee0624eab3c9c5e3c0396bafd537dcd
Rayanjunaidkhan <117000482+Rayanjunaidkhan@users.noreply.github.com>
2025-02-05 16:51:33 +0500

    Worked on bubblesort

---
+++ bubblesort.py
@@ -0,0 +1,14 @@
+nums = [32,13,142,213,65,453,231,786,564,1000]
+
+
+def bubblesort(nums):
+    n = len(nums)
+    for i in range(n):
+        for j in range(n-i-1):
+            if nums[j] > nums[j + 1]:
+                temp = nums[j]
+                nums[j] = nums[j + 1]
+                nums[j + 1] = temp
+              
+bubblesort(nums)
+print(nums)
