9322c7ca9dec266ee598c1ff81a69405b91d49ac
Rayanjunaidkhan <117000482+Rayanjunaidkhan@users.noreply.github.com>
2025-02-15 16:31:15 +0500

    did some oop practise

---
+++ ooppractise1.py
@@ -0,0 +1,23 @@
+class tree:
+    def __init__(self,treename,heightgrowth,maxheight,maxwidth,evergreen):
+        self.treename = treename
+        self.heightgrowth = heightgrowth
+        self.maxheight = maxheight
+        self.maxwidth = maxwidth
+        self.evergreen = evergreen
+
+        def gettreename(self):
+            return self.treename
+        
+        def getgrowth(self):
+            return self.heightgrowth
+        
+        def getmaxheight(self):
+            return self.maxheight
+        
+        def getmaxwidth(self):
+            return self.maxwidth
+        
+        def getevergreen(self):
+            return self.evergreen
+
